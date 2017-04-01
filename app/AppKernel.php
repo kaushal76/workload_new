0a994a232626f9cdcf32a985523fe700d3c796d4
Kausha Keraminiyage <keraminiyage@yahoo.co.uk>
2017-04-01 17:31:40 +0100

    FosUserBundle is added for security features

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new AppBundle\AppBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
